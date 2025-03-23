c643bbc572b31eb4244a744312a911b5c0c9adfb
Brian Zhou <brianzhou1102@gmail.com>
2025-03-22 21:20:46 -0400

    Fix test gen template code

--- test_gen_template.py
+++ test_gen_template.py
@@ -1,10 +1,10 @@
+from os.path import join
 import random
 import sys
 from time import perf_counter
 
 
-INPUT_FILE_PATH = ""
-OUTPUT_FOLDER_PATH = ""
+FOLDER_PATH = ""
 TESTS: list[list | str] = [
     [],  # configs for the inputs should be set here. Should be a list
     "",  # a full test can also be placed here as needed, this can be used to test weird edge cases 
@@ -22,16 +22,22 @@ def generate(*args):
     """Generates a test case. Expects an output from std.out."""
     if len(args) == 1 and type(args[0]) == str:
         print(args[0])  # just printout the pre-written testcase
+        return
     ...
 
 
 def main():
     for test_num, test in enumerate(TESTS):
-        with open(INPUT_FILE_PATH, mode='w') as sys.stdout:
-            generate(*test)
+        input_path = join(FOLDER_PATH, f"{test_num}in.txt")
+        output_path = join(FOLDER_PATH, f"{test_num}out.txt")
+        with open(input_path, mode='w') as sys.stdout:
+            if isinstance(test, str):
+                generate(test)
+            else:
+                generate(*test)
         start_time = perf_counter()
-        with open(OUTPUT_FOLDER_PATH + f"{test_num}.txt", mode='w') as sys.stdout:
-            with open(INPUT_FILE_PATH) as sys.stdin:
+        with open(output_path, mode='w') as sys.stdout:
+            with open(input_path) as sys.stdin:
                 solution()
         end_time = perf_counter()
         assert end_time - start_time < 10, "It should not take more than 10s to solve the testcase."
